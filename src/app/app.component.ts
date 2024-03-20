d35cc6ce36a77f7c5619dd5a8233d19f9ec4b0f2
PonssDev <alanponstrabajo@gmail.com>
2024-03-20 15:26:44 +0100

    Curso Angular. Directivas IV. Vídeo 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+
+  empleados: Empleado[] = [
+    new Empleado('Juan', 'Días', 'Presidente', 7500),
+    new Empleado('Ana', 'martín', 'Directora', 5500),
+    new Empleado('María', 'Fdez', 'Jefa seccion', 3500),
+    new Empleado('Laura', 'López', 'Administrativo', 2500),
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    this.empleados.push(miEmpleado)
+  }
+
+  cuadroNombre:string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario: number = 0;
 }
