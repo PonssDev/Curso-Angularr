0e178201e42c61aa41871f4af7dd870b78972ff0
PonssDev <alanponstrabajo@gmail.com>
2024-03-21 11:50:21 +0100

    Curso Angular. Routing II. Vídeo 25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,5 @@ import { EmpleadosService } from './empleados.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  titulo = 'Listado de empleados';
 
-  constructor(private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService){
-    this.empleados = this.empleadosService.empleados
-  }
-
-  empleados:Empleado[]=[]
-
-  agregarEmpleado(){
-    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
-    //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre)
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado)
-
-  }
-
-  cuadroNombre:string = '';
-  cuadroApellido: string = '';
-  cuadroCargo: string = '';
-  cuadroSalario: number = 0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,33 @@ import { EmpleadoHicoCComponent } from './empleado-hico-c/empleado-hico-c.compon
 import { CaracteristicasEmpleadoCComponent } from './caracteristicas-empleado-c/caracteristicas-empleado-c.component';
 import { ServicioEmpleadosService } from './servicio-empleados.service';
 import { EmpleadosService } from './empleados.service';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { QuienesComponent } from './quienes/quienes.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes=[
+  {path:'', component:HomeComponentComponent},
+  {path:'proyectos', component:ProyectosComponent},
+  {path:'quienes', component:QuienesComponent},
+  {path:'contacto', component:ContactoComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoHicoCComponent,
-    CaracteristicasEmpleadoCComponent
+    CaracteristicasEmpleadoCComponent,
+    HomeComponentComponent,
+    ProyectosComponent,
+    QuienesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServicioEmpleadosService, EmpleadosService],
   bootstrap: [AppComponent]
