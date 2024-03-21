2fbcd1f73fb1d6aea8ae6b06f5e7e92ccea471f4
PonssDev <alanponstrabajo@gmail.com>
2024-03-21 12:43:51 +0100

    Curso Angular. Routing III. Vídeo 26

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent {
+
+  constructor(private router:Router, private empleadosService: EmpleadosService){
+    this.empleados = this.empleadosService.empleados
+  }
+
+  volverHome(){
+    this.router.navigate([''])
+  }
+
+  empleados:Empleado[]=[]
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre)
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado)
+    this.router.navigate([''])
+
+  }
+
+  cuadroNombre:string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario: number = 0;
 
 }
