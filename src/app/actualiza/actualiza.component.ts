100a84c3388f072de7b9005eea9a06f836b8d861
PonssDev <alanponstrabajo@gmail.com>
2024-03-22 09:16:27 +0100

    Curso Angular. Routing VII. Vídeo 30

--- src/app/actualiza/actualiza.component.ts
+++ src/app/actualiza/actualiza.component.ts
@@ -6,29 +6,40 @@ import { Empleado } from '../empleado.model';
 @Component({
   selector: 'app-actualiza',
   templateUrl: './actualiza.component.html',
-  styleUrls: ['./actualiza.component.css']
+  styleUrls: ['./actualiza.component.css'],
 })
 export class ActualizaComponent {
-  constructor(private router:Router, private empleadosService: EmpleadosService, private route:ActivatedRoute){
-    this.empleados = this.empleadosService.empleados
+  constructor(
+    private router: Router,
+    private empleadosService: EmpleadosService,
+    private route: ActivatedRoute
+  ) {}
 
-    this.indice = this.route.snapshot.params['id']
-
-    let empleado: Empleado = this.empleadosService.encontrarEmpleado(this.indice)
-
-    this.cuadroNombre = empleado.nombre
-    this.cuadroApellido = empleado.apellido
-    this.cuadroCargo = empleado.cargo
-    this.cuadroSalario = empleado.salario
+  volverHome() {
+    this.router.navigate(['']);
   }
 
-  volverHome(){
-    this.router.navigate([''])
+  empleados: Empleado[] = [];
+  accion: number;
+
+  ngOnInit(): void {
+    this.accion = parseInt(this.route.snapshot.queryParams['accion']);
+
+    this.empleados = this.empleadosService.empleados;
+
+    this.indice = this.route.snapshot.params['id'];
+
+    let empleado: Empleado = this.empleadosService.encontrarEmpleado(
+      this.indice
+    );
+
+    this.cuadroNombre = empleado.nombre;
+    this.cuadroApellido = empleado.apellido;
+    this.cuadroCargo = empleado.cargo;
+    this.cuadroSalario = empleado.salario;
   }
 
-  empleados:Empleado[]=[]
-
-  actualizaEmpleado(){
+  /* actualizaEmpleado(){
     let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
     //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre)
     this.empleadosService.actualizarEmpleado(this.indice, miEmpleado)
@@ -40,12 +51,29 @@ export class ActualizaComponent {
     this.empleadosService.eliminarEmpleado(this.indice)
     this.router.navigate([''])
 
+  } */
+
+  actualizaEmpleado() {
+    if (this.accion == 1) {
+      let miEmpleado = new Empleado(
+        this.cuadroNombre,
+        this.cuadroApellido,
+        this.cuadroCargo,
+        this.cuadroSalario
+      );
+      //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre)
+      this.empleadosService.actualizarEmpleado(this.indice, miEmpleado);
+      this.router.navigate(['']);
+    } else {
+      this.empleadosService.eliminarEmpleado(this.indice);
+      this.router.navigate(['']);
+    }
   }
 
-  cuadroNombre:string = '';
+  cuadroNombre: string = '';
   cuadroApellido: string = '';
   cuadroCargo: string = '';
   cuadroSalario: number = 0;
 
-  indice:number;
+  indice: number;
 }
